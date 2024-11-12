2bab2cac6117e6cb77525ce95b8974010c97c6dc
changi123 <133079671+changi123@users.noreply.github.com>
2024-11-13 00:36:01 +0900

    Feat : 강의 디자인 코딩 완료

--- App.js
+++ App.js
@@ -6,11 +6,18 @@ export default function App() {
   const [number,setNumber] = useState(0);
 
   return (
-    <View style={styles.container}>
-      <Text style = {{fontSize: 50}}>결과 : {number}</Text>
-      <View style= {{flexDirection:"row" , gap:10}}>
-        <Button title='증가' onPress={() => setNumber(number+1)}></Button>
-        <Button title='감소' onPress={() => setNumber(number-1)}></Button>
+    <View style={ styles.container }>
+      <View style={styles.cityCon}>
+      <Text style ={styles.city}>Ansan</Text>
+      </View>
+      <View style={styles.weatherCon}>
+        <View style = {styles.day}>
+          <Text style ={styles.regDate}>11월 12일, 화, 23:25</Text>
+          <Text style ={styles.desc}>맑음</Text>
+        </View>
+          <View style={styles.tempCon}>
+          <Text style ={styles.temp}>24</Text>
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -18,10 +25,62 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container : {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffe01a',
+  },
+  
+  cityCon : {
+    flex : 1 ,
+  },
+  
+  city : {
+    flex : 1,
+    marginTop: 50,
+    paddingTop : 20,
+    fontSize: 40,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+
+  weatherCon : {
+    flex : 3 ,
+    
+  }, 
+
+  day : {
+    flex : 0.2,
+    alignItems : 'center',
+    justifyContent : 'center',
+  },
+
+  regDate : {
+    backgroundColor : "black",
+    color : "white",
+    borderRadius : 20,
+    overflowY : "hidden",
+    fontWeight : "bold",
+    padding : 10,
+    paddingTop : 10 ,
+    paddingLeft : 20,
+    paddingRight : 20,
+    paddingBottom : 15,
+  },
+  desc : {
+    flex : 1.5,
+    marginTop : 20,
+    fontSize : 30,
+    fontWeight : "bold",
+  },
+  
+  tempCon : {
+    flex : 0.3,
+    alignItems : 'center',
+    justifyContent : 'center',
+
+  },
+
+  temp : {
+    fontSize : 120,
   },
 });
